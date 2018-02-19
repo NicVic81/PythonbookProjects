e9023f6ba79c51df48a18c3ddcbc022a1803faad
NicVic81 <nic81@suddenlink.net>
2018-02-19 00:17:55 -0500

    Chapter 8 finishing for the night

--- Chapter8_Functions.py
+++ Chapter8_Functions.py
@@ -1,5 +1,15 @@
+#def is what you use to define a function
 def greet_user():
+    #This is called a docstring and is used in the documentation of a function
     """Display a simple greeting"""
     print("Hello!")
 
+#This just calls the function
 greet_user()
+
+#We can now do the same thing but use the () to pass in a value
+def greet_user(username):
+    """Display a simple greeting"""
+    print("Hello, " + username.title() + "!")
+
+greet_user('jesse')
