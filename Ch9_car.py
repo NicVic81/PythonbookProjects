0a545e702fbebea844573704c35ac9eee27b8708
nicvic81 <nic81addison@gmail.com>
2018-03-26 23:08:19 -0400

    Finishing up in bed on 3/26/2018

--- Ch9_car.py
+++ Ch9_car.py
@@ -1,31 +1,69 @@
-class Car:
-    """A simple attempt to represent a car."""
+# class Car:
+#     """A simple attempt to represent a car."""
+#
+#     def __init__(self, make, model, year):
+#         """Initialize attributes to describe a car|"""
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#         self.odometer_reading = 0
+#
+#     def get_descriptive_name(self):
+#         """Return a neatly formatted descriptive name"""
+#         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+#         return long_name.title()
+#
+#     def read_odometer(self):
+#         """Show the car's mileage"""
+#         print("This cas has " + str(self.odometer_reading) + " miles on it")
+#
+#
+# my_new_car = Car('audi', 'a4', 2016)
+# print(my_new_car.get_descriptive_name())
+# # You could update the odometer reading manually by doing
+# my_new_car.odometer_reading = 28
+# my_new_car.read_odometer()
+#
+#
+# # Another way of updating an attribute is through a method
+#
+# class Car:
+#     """A simple attempt to represent a car."""
+#
+#     def __init__(self, make, model, year):
+#         """Initialize attributes to describe a car|"""
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#         self.odometer_reading = 0
+#
+#     def get_descriptive_name(self):
+#         """Return a neatly formatted descriptive name"""
+#         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+#         return long_name.title()
+#
+#     def update_odometer(self, mileage):
+#         """
+#         Set the odometer reading to the given value
+#         Reject the change if it attempts to roll the odometer back.
+#         """
+#         if mileage >= self.odometer_reading:
+#             self.odometer_reading = mileage
+#         else:
+#             print("You can't roll back an odometer")
+#
+#     def read_odometer(self):
+#         """Show the car's mileage"""
+#         print("This cas has " + str(self.odometer_reading) + " miles on it")
+#
+#
+# my_new_car = Car('audi', 'a4', 2016)
+# print(my_new_car.get_descriptive_name())
+# my_new_car.update_odometer(150)
+# my_new_car.read_odometer()
 
-    def __init__(self, make, model, year):
-        """Initialize attributes to describe a car|"""
-        self.make = make
-        self.model = model
-        self.year = year
-        self.odometer_reading = 0
+# Updating the mileage by a certain amount
 
-    def get_descriptive_name(self):
-        """Return a neatly formatted descriptive name"""
-        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
-        return long_name.title()
-
-    def read_odometer(self):
-        """Show the car's mileage"""
-        print("This cas has " + str(self.odometer_reading) + " miles on it")
-
-
-my_new_car = Car('audi', 'a4', 2016)
-print(my_new_car.get_descriptive_name())
-# You could update the odometer reading manually by doing
-my_new_car.odometer_reading = 28
-my_new_car.read_odometer()
-
-
-# Another way of updating an attribute is through a method
 
 class Car:
     """A simple attempt to represent a car."""
@@ -54,10 +92,21 @@ class Car:
 
     def read_odometer(self):
         """Show the car's mileage"""
-        print("This cas has " + str(self.odometer_reading) + " miles on it")
+        print("This car has " + str(self.odometer_reading) + " miles on it")
+
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        if miles > 0:
+            self.odometer_reading += miles
+        else:
+            print("You can't roll back an odometer")
 
 
-my_new_car = Car('audi', 'a4', 2016)
-print(my_new_car.get_descriptive_name())
-my_new_car.update_odometer(150)
-my_new_car.read_odometer()
+my_used_car = Car('subaru', 'outback', 2013)
+print(my_used_car.get_descriptive_name())
+
+my_used_car.update_odometer(23500)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
